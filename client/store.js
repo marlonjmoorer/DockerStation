1107fff2d5988da5b9aff09c12283f15c157ae2d
marlonmoorer <marlonjmoorer@gmail.com>
2018-10-24 17:15:01 -0400

    dev tools update

--- client/store.js
+++ client/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware,compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+const devtools=window.devToolsExtension ? window.devToolsExtension() : f => f;
 export default function configureStore() {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
  return createStore(
    rootReducer,
-   composeEnhancers(applyMiddleware(thunk))
+   compose(applyMiddleware(thunk),devtools)
  );
 }
+
